00e36183ed7f08ba52a5a6f26d69eb546f6b4bd9
Andrew Thometz <148142545+andrewthometz@users.noreply.github.com>
2024-08-16 08:19:26 -0500

    Update and rename PeakMorph_2111.R to Genotype_Peak_Morphologies.R

--- Genotype_Peak_Morphologies.R
+++ Genotype_Peak_Morphologies.R
@@ -1,8 +1,15 @@
+# Load packages
 library(tidyverse)
 library(ggforce)
 
-### Function to make length_dist files tidy ###
+#########################################################################################
+#### Produce multi-page pdf document containing peak morphology plots for each locus ####
+#########################################################################################
 
+# Each locus (n = 91) should have its own page with peak morphology plots from 12 random fish
+# This allows us to assess which loci are consistently producing clean allele calls (locus trustworthiness)
+
+#### Write function to make genotype call frequency files tidy ####
 length_dist_tidy <- function(x){
   
   BKT_ID <- deparse(substitute(x))
@@ -18,8 +25,7 @@ length_dist_tidy <- function(x){
   print(output)
 }
 
-### Read in 12 BKT ### Grabbed largest few files from each cat folder. Used some from each folder to get "even" representation
-
+#### Read in 12 random fish (Grabbed largest few files from each cat folder. Used some from each folder to get "even" representation) ####
 BKT_21_08632 <- read_delim("X:/2111_F1F2D_BKT/MEGAsat_outputs/MEGAsat_2111_cat_1of5_output/length_distribution/Genotype_21-08632.txt")
 BKT_21_08633 <- read_delim("X:/2111_F1F2D_BKT/MEGAsat_outputs/MEGAsat_2111_cat_1of5_output/length_distribution/Genotype_21-08633.txt")
 BKT_21_08624 <- read_delim("X:/2111_F1F2D_BKT/MEGAsat_outputs/MEGAsat_2111_cat_1of5_output/length_distribution/Genotype_21-08624.txt")
@@ -37,8 +43,7 @@ BKT_22_11380 <- read_delim("X:/2111_F1F2D_BKT/MEGAsat_outputs/MEGAsat_2111_cat_4
 BKT_22_11777 <- read_delim("X:/2111_F1F2D_BKT/MEGAsat_outputs/MEGAsat_2111_cat_5of5_output/length_distribution/Genotype_22-11777.txt")
 BKT_22_11658 <- read_delim("X:/2111_F1F2D_BKT/MEGAsat_outputs/MEGAsat_2111_cat_5of5_output/length_distribution/Genotype_22-11658.txt")
 
-### Run each fish's length_dist file through my tidying function while joining them ###
-
+#### Run each fish's length_dist file through my tidying function while joining them ####
 All_LD_data <- bind_rows(length_dist_tidy(BKT_21_08632),
                          length_dist_tidy(BKT_21_08633),
                          length_dist_tidy(BKT_21_08624),
@@ -52,17 +57,28 @@ All_LD_data <- bind_rows(length_dist_tidy(BKT_21_08632),
                          length_dist_tidy(BKT_22_11777),
                          length_dist_tidy(BKT_22_11658))
 
+#### Create a single page of peak morphology plots to quickly test changes ####
 All_LD_data %>% 
   #filter(Read_count > 0) %>% 
   group_by(uSat_locus, SampleID) %>% 
   #slice_max(Read_count, n = 10) %>% 
-  filter(Read_count >= max(Read_count)*0.25) %>% 
-  ungroup() %>% 
-  summarize(x = n_distinct(uSat_locus))
+  filter(Read_count >= max(Read_count)*0.05) %>% 
+  ggplot(aes(x = Length, y = Read_count)) +
+  geom_col(fill = "seagreen") +
+  geom_text(aes(label = Length), hjust = 0.5, vjust = 1.5, colour = "black") +
+  ylab("Read count") +
+  xlab("Allele length") +
+  scale_y_continuous(expand = c(0,0)) +
+  #scale_x_continuous(breaks = c(0:200), expand = c(0,0)) +
+  theme_classic() +
+  facet_wrap_paginate(facets = vars(as.factor(uSat_locus), as.factor(SampleID), as.factor(scores)), 
+                      nrow = 3,
+                      ncol = 4,
+                      page = 49, # Random page
+                      scales = "free")
 
-### Make peak morphology plots, one locus and 12 BKT per page ### Saves directly as pdf
-
-# Beware, this takes about 40min to run #
+#### Create loop to produce peak morphology plots, one locus and 12 fish per page. Save directly as pdf ####
+# Beware, this takes about 40 minutes to run #
 
 pdf("X:/2111_F1F2D_BKT/2111analysis/Thometz_scripts/2111_PeakMorph_Thometz.pdf", paper = "a4r", width = 11, height = 9)
 
@@ -91,28 +107,7 @@ print(All_LD_data %>%
                             page = i, 
                             scales = "free"))
   Sys.sleep(0.1)
-  setTxtProgressBar(ProgressBar, i)
+  setTxtProgressBar(ProgressBar, i) # Progress bar allows you to monitor the loop's progress
 }
 close(ProgressBar)
 dev.off()
-
-### Single page of peak morphs for testing plot changes ###
-
-All_LD_data %>% 
-  #filter(Read_count > 0) %>% 
-  group_by(uSat_locus, SampleID) %>% 
-  #slice_max(Read_count, n = 10) %>% 
-  filter(Read_count >= max(Read_count)*0.05) %>% 
-  ggplot(aes(x = Length, y = Read_count)) +
-  geom_col(fill = "seagreen") +
-  geom_text(aes(label = Length), hjust = 0.5, vjust = 1.5, colour = "black") +
-  ylab("Read count") +
-  xlab("Allele length") +
-  scale_y_continuous(expand = c(0,0)) +
-  #scale_x_continuous(breaks = c(0:200), expand = c(0,0)) +
-  theme_classic() +
-  facet_wrap_paginate(facets = vars(as.factor(uSat_locus), as.factor(SampleID), as.factor(scores)), 
-                      nrow = 3,
-                      ncol = 4,
-                      page = 49, 
-                      scales = "free")
