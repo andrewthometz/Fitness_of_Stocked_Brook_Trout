3f857db80dd50c02e99b5b89c90fa9d3db7d34ad
Andrew Thometz <148142545+andrewthometz@users.noreply.github.com>
2024-08-16 08:01:41 -0500

    Update and rename Offspring_length_frequencies.R to Offspring_Length_Frequency_Plots.R

--- Offspring_Length_Frequency_Plots.R
+++ Offspring_Length_Frequency_Plots.R
@@ -1,10 +1,18 @@
+# Load packages
 library(tidyverse)
 library(readxl)
 library(devtools)
 devtools::install_github("thomasp85/patchwork")
 library(patchwork)
 
-#### Create length-frequency distributions for each wild cohort ####
+###############################################################################
+#### Create length-frequency distributions for each year of wild offspring ####
+###############################################################################
+
+# Wild offspring cohorts indicate the year a fish was believed to have been naturally produced
+# For example, wild offspring captured in 2020 are in the W_2019 cohort because the natural reproduction would have occured in Fall of 2019
+
+# Read in metadata
 Offspring_2111 <- read_delim("X:/2111_F1F2D_BKT/2111analysis/Thometz_scripts/Samples_2111.csv") %>% 
   filter(Cohort_year == "W_2019" |
          Cohort_year == "W_2020" |
@@ -14,14 +22,14 @@ Offspring_2111 <- read_delim("X:/2111_F1F2D_BKT/2111analysis/Thometz_scripts/Sam
                              Capture_date == "Apr-5-2021" ~ "Spring 2021",
                              Capture_date == "Sep-8-2021" ~ "Fall 2021",
                              Capture_date == "Apr-4-2022" ~ "Spring 2022",
-                             Capture_date == "Oct-6-2022" ~ "Fall 2022"))
+                             Capture_date == "Oct-6-2022" ~ "Fall 2022")) # Simplify capture date to season + year
 
 Offspring_2111 %>% count(Capture_date)
 Offspring_2111 %>% count(Cohort)
 
-# Offspring from W_2020 cohort (Don't have length data for fish captured on 4/5/2020 (n = 28)) Doesn't matter...
+# Plot for offspring from W_2019 cohort (We don't have length data for fish captured on 4/5/2020 (n = 28)... not a big deal though)
 plot_2019 <- Offspring_2111 %>% 
-  filter(Capture_date == "Sep-8-2020") %>% # Don't have length data for Apr-5-2021 (n = 28 fish)
+  filter(Capture_date == "Sep-8-2020") %>%
   ggplot(aes(x = TotalLength, fill = Capture)) +
   geom_histogram(binwidth = 20,
                  center = 10,
@@ -41,7 +49,7 @@ plot_2019 <- Offspring_2111 %>%
   theme(legend.position = "top",
         plot.margin = margin(0, 10, 0, 0))
 
-# Offspring from W_2021 cohort
+# Plot for offspring from W_2020 cohort
 plot_2020 <- Offspring_2111 %>% 
   filter(Cohort_year == "W_2020") %>% 
   ggplot(aes(x = TotalLength, fill = Capture)) +
@@ -63,7 +71,7 @@ plot_2020 <- Offspring_2111 %>%
   theme(legend.position = "top",
         plot.margin = margin(0, 10, 0, 0))
 
-# Offspring from W_2022 cohort
+# Plot for offspring from W_2021 cohort
 plot_2021 <- Offspring_2111 %>% 
   filter(Cohort_year == "W_2021") %>% 
   ggplot(aes(x = TotalLength, fill = Capture)) +
@@ -85,6 +93,7 @@ plot_2021 <- Offspring_2111 %>%
   theme(legend.position = "top",
         plot.margin = margin(0, 10, 0, 0))
 
+# Join the three plots together using the patchwork package
 length_freqs <- plot_2019 + plot_2020 + plot_2021
 
 ggsave(filename = "Wild_length_frequencies.pdf",
