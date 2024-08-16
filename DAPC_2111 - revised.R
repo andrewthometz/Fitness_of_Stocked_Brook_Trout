4d7ea2a60136c6717254fcf10454f45284ba476d
Andrew Thometz <148142545+andrewthometz@users.noreply.github.com>
2024-08-16 07:40:16 -0500

    Update DAPC_2111 - revised.R

--- DAPC_2111 - revised.R
+++ DAPC_2111 - revised.R
@@ -1,11 +1,15 @@
+# Load packages
 library(tidyverse)
 library(readxl)
 library(adegenet)
 library(poppr)
 library(ggrepel)
 
-# DAPC tests a hypothesis, PCA does not
+########################################################################################################################
+#### Run Discriminant Analysis of Principle Components (DAPC) to assess how offspring assign to each stocked cohort ####
+########################################################################################################################
 
+# Remember DAPC tests a hypothesis, PCA does not
 # DAPC guidelines from Thia 2022:
 # n.da = k groups (should be determined a priori, # of sample pops)
 # n.pca must be =< k-1 (only k-1 PCs are biologically informative)
@@ -15,14 +19,14 @@ Data_2111 <- read.genepop("X:/2111_F1F2D_BKT/2111analysis/Thometz_scripts/2111_g
                           ncode = 3L, 
                           quiet = FALSE)
 
-#### Read in 2111 metadata ####
+#### Read in metadata ####
 Samples_2111 <- read_delim("X:/2111_F1F2D_BKT/2111analysis/Thometz_scripts/Samples_2111.csv") %>% 
   filter(SampleID %in% rownames(Data_2111@tab)) %>% 
   arrange(match(SampleID, rownames(Data_2111@tab))) %>% 
   mutate(WaterbodyName = case_when(str_detect(WaterbodyName, "St. Croix") ~ "St. Croix",
                                    .default = WaterbodyName))
 
-#### subset to genetic data by Cohort ####
+#### Subset genind objects by Cohort ####
 Data_2111@pop <- as_factor(Samples_2111$Cohort)
 
 Stocked_fish <- popsub(Data_2111, 
@@ -33,7 +37,7 @@ Wild_offspring <- popsub(Data_2111,
                          sublist = c("Wild"),
                          drop = FALSE)
 
-#### Run DAPC using just stocked fish ####
+#### Run DAPC using just stocked fish (potential parents) ####
 set.seed(27)
 clusters_stocked <- find.clusters.genind(Stocked_fish, 
                                          max.n.clust = 5,
@@ -53,7 +57,7 @@ DAPC_stocked <- dapc.genind(Stocked_fish,
 
 summary(DAPC_stocked)
 
-# Create figure for reassignment percentages
+# Create plot to depict reassignment percentages
 reassignment_plot <- summary(DAPC_stocked)$assign.per.pop %>% 
   as_tibble(rownames = "Cohort") %>% 
   mutate(reassignment = round(value*100, 0),
@@ -86,16 +90,18 @@ ggsave(filename = "reassignment_plot.png",
        height = 4,
        width = 6,
        units = "in")
-  
+
+# Calculate A score to identify optimal number of PCs
 A_score_stocked <- optim.a.score(DAPC_stocked) # Optimal n.pca = 2
 
-# Bring wild offspring in as supplementary individuals to assign back to stocked cohorts
+# Introduce wild offspring in as supplementary individuals to assign back to stocked cohorts
 prediction <- predict.dapc(DAPC_stocked, newdata = Wild_offspring)
 
 prediction$assign
 prediction$posterior
 prediction$ind.scores
 
+# Extract and clean up the assignment probabilities
 Assignment_probs <- prediction$posterior %>%
   as_tibble(rownames = "SampleID") %>% 
   pivot_longer(cols = 2:4, 
@@ -139,7 +145,7 @@ ggsave(filename = "DAPC_ass_prob_plot.png",
        width = 8,
        units = "in")
 
-#### Try plotting like PCA ####
+#### Create ordination plot ####
 # Make tibble from original DAPC results
 row_names <- row.names(DAPC_stocked$grp.coord)
 
@@ -151,8 +157,7 @@ dapc_centroids$Cohort <- row_names
 stocked_centroids <- dapc_centroids %>% 
   select(Cohort) %>% 
   right_join(aggregate(cbind(LD1, LD2) ~ Cohort, dapc_centroids, mean)) %>% 
-  distinct(.keep_all = TRUE) #%>% 
-  #mutate(Cohort = str_replace_all(Cohort, "_", " "), .keep = "unused")
+  distinct(.keep_all = TRUE)
 
 # Make tibble from prediction results
 row_names <- row.names(prediction$ind.score)
@@ -232,114 +237,3 @@ ggsave(filename = "DAPC_ord_by_cohort.png",
        height = 5,
        width = 8,
        units = "in")
-
-##################################################### Not feasible ##############
-#### Re-run DAPC by source stream ####
-Data_2111@pop <- as_factor(Samples_2111$WaterbodyName)
-
-Stocked_fish <- popsub(Data_2111, 
-                       exclude = "Strutt Creek",
-                       drop = FALSE)
-
-Wild_offspring <- popsub(Data_2111, 
-                         sublist = "Strutt Creek",
-                         drop = FALSE)
-
-#### Run DAPC using just stocked fish ####
-set.seed(27)
-clusters_stocked <- find.clusters.genind(Stocked_fish, 
-                                         max.n.clust = 12,
-                                         n.pca = 250,
-                                         n.clust = 4)
-
-# Select 250 to retain all PCs
-# Selecting 4 clusters, lowest BIC
-
-table(Stocked_fish$pop, clusters_stocked$grp)
-
-DAPC_stocked <- dapc.genind(Stocked_fish, 
-                            #pop = clusters_hybrids$grp,
-                            #n.clust = 4,
-                            n.pca = nPop(Stocked_fish) - 1,
-                            n.da = nPop(Stocked_fish))
-
-summary(DAPC_stocked)
-
-A_score_stocked <- optim.a.score(DAPC_stocked) # Optimal n.pca = 6
-
-# Bring wild offspring in as supplementary individuals to assign back to stocked cohorts
-prediction <- predict.dapc(DAPC_stocked, newdata = Wild_offspring)
-
-prediction$assign
-prediction$posterior
-prediction$ind.scores
-
-Assignment_probs <- round(prediction$posterior, 2) %>%
-  as_tibble(rownames = "Offspring_ID") %>% 
-  #mutate(Offspring_year = Wild_offspring@pop) %>% 
-  pivot_longer(cols = 2:8, 
-               names_to = "Cluster", 
-               values_to = "Probability")
-
-# Plot all assignment probabilities
-Assignment_probs %>% 
-  ggplot(aes(Offspring_ID, Probability, fill = factor(Cluster))) +
-  geom_col(color = "gray", linewidth = 0.1) +
-  #facet_grid(~fct_inorder(Offspring_year), 
-  #           switch = "x", 
-  #           scales = "free", 
-  #           space = "free") +
-  labs(x = "Offspring", y = "Assignment probability") +
-  scale_y_continuous(expand = c(0, 0)) +
-  scale_x_discrete(expand = expansion(add = 1)) +
-  scale_fill_discrete("Dark2") +
-  theme_minimal(base_size = 15) + 
-  theme(panel.spacing.x = unit(0.01, "lines"),
-        axis.text.x = element_blank(),
-        panel.grid = element_blank()) + 
-  guides(fill = guide_legend(title = "Genetic lineage")) +
-  theme(strip.text.x = element_text(angle = -90))
-
-#### Plotting like PCA ####
-# Make tibble from prediction results
-row_names <- row.names(prediction$ind.score)
-
-prediction_data <- prediction$ind.scores %>% 
-  as_tibble()
-
-prediction_data$SampleID <- row_names
-
-prediction_data <- prediction_data %>% 
-  left_join(Samples_2111)
-
-# Make tibble from original DAPC results
-row_names <- row.names(DAPC_stocked$grp.coord)
-
-dapc_data <- DAPC_stocked$grp.coord %>% 
-  as_tibble()
-
-dapc_data$WaterbodyName <- row_names
-
-# Plot
-centroids <- dapc_data %>% 
-  select(WaterbodyName) %>% 
-  right_join(aggregate(cbind(LD1, LD2) ~ WaterbodyName, dapc_data, mean)) %>% 
-  distinct(.keep_all = TRUE)
-
-prediction_data %>% 
-  ggplot(aes(x = LD1, y = LD2, color = WaterbodyName)) + 
-  geom_point(alpha = 0.4) + 
-  stat_ellipse() + 
-  geom_point(data = centroids, 
-             size = 6, 
-             alpha = 0.8) + 
-  geom_text_repel(data = centroids, 
-                  aes(label = WaterbodyName), 
-                  fontface = "bold", 
-                  size = 5, force = 5, 
-                  force_pull = 0.1, 
-                  max.overlaps = 100) + 
-  ylab("Axis 2") + 
-  xlab("Axis 1") + 
-  theme_classic(base_size = 18) + 
-  theme(legend.position = "none")
